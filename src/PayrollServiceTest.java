6a19c147a3206f50b2aea0d854f7c9fc679d1d38
YSK <nightmare.zgtk1220@gmail.com>
2026-07-25 22:35:59 +0900

    休憩適用条件のテストを追加

--- src/PayrollServiceTest.java
+++ src/PayrollServiceTest.java
@@ -1,20 +1,38 @@
 public class PayrollServiceTest {
     public static void main(String[] args) {
         PayrollService service = new PayrollService();
-        Config config = new Config("テスト", 1200, 8 * 60, 0);
+        Config noBreakConfig = new Config("テスト", 1200, 8 * 60, 0);
+        Config standardBreakConfig = new Config("テスト", 1200, 8 * 60, 60);
 
         WorkRecord daytime = service.calculate(
-                "2026-07-25", config, new MyTime("9:00"), new MyTime("17:00"));
+                "2026-07-25", noBreakConfig,
+                new MyTime("9:00"), new MyTime("17:00"));
         assertEquals(8 * 60, daytime.getWorkMinutes());
 
         WorkRecord overnight = service.calculate(
-                "2026-07-25", config, new MyTime("22:00"), new MyTime("6:00"));
+                "2026-07-25", noBreakConfig,
+                new MyTime("22:00"), new MyTime("6:00"));
         assertEquals(8 * 60, overnight.getWorkMinutes());
 
         WorkRecord overnightWithBreak = service.calculate(
-                "2026-07-25", new Config("テスト", 1200, 8 * 60, 60),
+                "2026-07-25", standardBreakConfig,
                 new MyTime("22:00"), new MyTime("6:00"));
         assertEquals(7 * 60, overnightWithBreak.getWorkMinutes());
+
+        WorkRecord shortShift = service.calculate(
+                "2026-07-25", standardBreakConfig,
+                new MyTime("10:00"), new MyTime("12:00"));
+        assertEquals(2 * 60, shortShift.getWorkMinutes());
+
+        WorkRecord exactlySixHours = service.calculate(
+                "2026-07-25", standardBreakConfig,
+                new MyTime("9:00"), new MyTime("15:00"));
+        assertEquals(6 * 60, exactlySixHours.getWorkMinutes());
+
+        WorkRecord overSixHours = service.calculate(
+                "2026-07-25", standardBreakConfig,
+                new MyTime("9:00"), new MyTime("15:01"));
+        assertEquals(5 * 60 + 1, overSixHours.getWorkMinutes());
 
         assertThrows(() -> new MyTime("24:00"));
         assertThrows(() -> new MyTime("30:00"));
